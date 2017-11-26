03e592720ed80cbf0ead21085e99f60e16fdec59
Abhimanyu Tyagi <abhimanyutyagi2@gmail.com>
2017-11-26 11:51:03 -0500

    Added an impact blast for projectile impact effect

--- BattleTank/Source/BattleTank/Projectile.h
+++ BattleTank/Source/BattleTank/Projectile.h
@@ -19,8 +19,6 @@ protected:
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
 
 	void LaunchProjectile(float Speed);
 
@@ -32,6 +30,13 @@ private:
 	UStaticMeshComponent* CollisionMesh = nullptr;
 
 	UPROPERTY(VisibleAnywhere, Category = "Setup")
-	UParticleSystemComponent* LaunchBlast;
+	UParticleSystemComponent* LaunchBlast = nullptr;
+
+	UPROPERTY(VisibleAnywhere, Category = "Setup")
+		UParticleSystemComponent* ImpactBlast = nullptr;
+
+	UFUNCTION()
+		void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
+
 	
 };
